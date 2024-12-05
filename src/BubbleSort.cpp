73771d3835526de6e71e32a40a8d2f65eb6d2295
clint <clinton_luo@qq.com>
2024-12-05 17:14:31 +0800

    1

--- src/BubbleSort.cpp
+++ src/BubbleSort.cpp
@@ -6,10 +6,10 @@
 void BubbleSort::sort(std::vector<int>& arr) {
     int len = arr.size();
     bool flag = false; // 内循环是否已经有序
-    for(int i = 0; i<=len-1 && !flag; i++){  
+    for(int i = 0; i<=len -1 && !flag; i++){  
         flag = true; // 设置默认为有序
         for(int  j=i; j<=len-1;j++){
-            if(arr[j] >arr[j+1]){
+            if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]); 
                 flag = false; // 检测，表示非有序
             }       
